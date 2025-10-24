47c2c564e1571abb3cc3b752968d3f38a6a90e8b
arthursteinerr <arthursteinerr@gmail.com>
2025-10-24 08:23:55 -0300

    Corrigindo o arquivo dbConnection.ts

--- src/dbConnection.ts
+++ src/dbConnection.ts
@@ -1,15 +1,17 @@
- import knex from "knex";
- import dotenv from "dotenv";
+// dbConnection.ts
+import knex from "knex";
+import dotenv from "dotenv";
 
- dotenv.config()
+dotenv.config();
 
- export const connection = knex({
-  client: process.env.DB_CLIENT,
+export const connection = knex({
+  client: "pg",
   connection: {
-      host: process.env.DB_HOST ,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      port: Number(process.env.DB_PORT)
-  }
-})
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  pool: { min: 0, max: 10 },
+});
