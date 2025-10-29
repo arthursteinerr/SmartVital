7aee77518f88ec3623d4732fab45aee49af78186
arthursteinerr <arthursteinerr@gmail.com>
2025-10-29 09:00:44 -0300

    Resolvendo corretamente o conflito no arquivo src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,10 @@
-// src/app.ts
 import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
 import { agenteRouter } from "./routes/agenteRoutes";
-<<<<<<< HEAD
-=======
-import { pacienteRouter } from "./routes/pacienteRoutes";
->>>>>>> 5228bbecae86713110b81cc5dce2db9ce23b7716
+import { pacienteRouter } from "./routes/pacienteRoutes"; 
 import { relatorioRouter } from "./routes/relatorioRoutes";
-import pacienteRouter from "./routes/pacienteRoutes";
 
 dotenv.config();
 
@@ -21,12 +16,8 @@ app.use(express.json());
 
 // Rotas principais
 app.use("/agentes", agenteRouter);
-<<<<<<< HEAD
-=======
 app.use("/pacientes", pacienteRouter);
->>>>>>> 5228bbecae86713110b81cc5dce2db9ce23b7716
 app.use("/relatorios", relatorioRouter);
-app.use("/pacientes", pacienteRouter); 
 
 // Verificar se a API está funcionando
 app.get("/", (req: Request, res: Response) => {
@@ -34,10 +25,7 @@ app.get("/", (req: Request, res: Response) => {
     message: "SmartVital API Funcionando!",
   });
 });
-<<<<<<< HEAD
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando na porta", process.env.PORT || 3000);
 });
-=======
->>>>>>> 5228bbecae86713110b81cc5dce2db9ce23b7716
