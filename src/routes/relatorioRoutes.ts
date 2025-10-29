de441e67ba2df6ecd8a0d41434139daf5d594906
Joao antonio <joaoif.eletro@gmail.com>
2025-10-29 14:50:27 -0300

    Remocao verbosidade nas rotas do relatorioRoutes

--- src/routes/relatorioRoutes.ts
+++ src/routes/relatorioRoutes.ts
@@ -6,12 +6,12 @@ export const relatorioRouter = Router();
 const relatorioController = new RelatorioController();
 
 // Metodos para busca
-relatorioRouter.get("/buscar/pendentes", (req, res) => relatorioController.listarPendentes(req, res));
-relatorioRouter.get("/buscar/:id", (req, res) => relatorioController.buscarPorId(req, res));
-relatorioRouter.get("/buscar/paciente/:id", (req, res) => relatorioController.listarPorPaciente(req, res));
+relatorioRouter.get("/pendentes", (req, res) => relatorioController.listarPendentes(req, res));
+relatorioRouter.get("/:id", (req, res) => relatorioController.buscarPorId(req, res));
+relatorioRouter.get("/paciente/:id", (req, res) => relatorioController.listarPorPaciente(req, res));
 
 // Metodo para criar
-relatorioRouter.post("/criar/relatorio", (req, res) => relatorioController.criar(req, res));
+relatorioRouter.post("/", (req, res) => relatorioController.criar(req, res));
 
 // Metodo para atualizar
-relatorioRouter.patch("/atualizar/:id", (req, res) => relatorioController.atualizar(req, res));
+relatorioRouter.patch("/:id", (req, res) => relatorioController.atualizar(req, res));
