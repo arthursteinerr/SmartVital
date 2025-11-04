cf285b071c69ddcb041004fbc498550215a7db79
arthursteinerr <arthursteinerr@gmail.com>
2025-11-04 14:39:25 -0300

    Adiciona paginação no agenteController.ts

--- src/controller/agenteController.ts
+++ src/controller/agenteController.ts
@@ -1,19 +1,46 @@
 import { Request, Response } from "express";
-import { getAllAgentesBusiness } from "../business/agenteBusiness";
 import { getAgenteByIdBusiness } from "../business/agenteBusiness";
 import { createAgenteBusiness } from "../business/agenteBusiness";
 import { updateAgenteBusiness } from "../business/agenteBusiness";
 import { deleteAgenteBusiness } from "../business/agenteBusiness";
+import { AgenteFilterDTO } from "../dto/agenteFilterDTO";
+import { getAllAgentes } from "../data/agenteData";
 
 //GET All Agentes
-export const getAllAgentesController = async (req: Request, res: Response) => {
-  const result = await getAllAgentesBusiness();
-  
-  if (!result.success) {
-    return res.status(404).json({ message: result.message });
-  }
+export const getAgentesController = async (req: Request, res: Response) => {
+  try {
+    const {
+      nome,
+      cargo,
+      registro_profissional,
+      page = "1",
+      limit = "10",
+      sortBy = "id",
+      sortOrder = "asc",
+    } = req.query;
 
-  return res.status(200).json(result.data);
+    const headerLimit = req.headers["limit"];
+    const headerPage = req.headers["page"];
+    const headerSortBy = req.headers["sortBy"]
+    const headerSortOrder = req.headers["sortOrder"]
+
+    const filter: Required<AgenteFilterDTO> = {
+      nome: (nome as string) || "",
+      cargo: (cargo as string) || "",
+      registro_profissional: (registro_profissional as string) || "",
+      page: Number(headerPage || page) || 1,
+      limit: Number(headerLimit || limit) || 10,
+      sortBy: (sortBy as string) as any,
+      sortOrder: (sortOrder as string) === "desc" ? "desc" : "asc",
+    };
+
+    const result = await getAllAgentes(filter);
+    res.status(200).json(result);
+
+  } catch (error: any) {
+      console.error("Erro no controller de agentes:", error);
+      res.status(500).json({ error: error.message });
+  }
 };
 
 //GET Agentes By ID
