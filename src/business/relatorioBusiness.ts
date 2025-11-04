59cc1265c445373a0b3a1a6b9df434d20f308118
Joao antonio <joaoif.eletro@gmail.com>
2025-11-04 15:31:22 -0300

    Adicionando os novos metodos ao relatorioBusiness.ts

--- src/business/relatorioBusiness.ts
+++ src/business/relatorioBusiness.ts
@@ -39,4 +39,40 @@ export class RelatorioBusiness {
 
         return await relatorioData.getRelatoriosPendentes();
     }
+
+    async listarPorData(data: string): Promise<Relatorio[]> {
+        
+        if(!/^\d{2}\/\d{2}\/\d{4}$/.test(data)){
+            throw new Error("Formatação da 'data' inválido. Use o formato DD/MM/YYYY.")
+        }
+        
+        // Convertendo a data do padrao brasileiro DD/MM/YYYY para o usado no banco de dados YYYY/MM/DD
+        const [dia, mes, ano] = data.split("/");
+        const dataFormatada = `${ano}-${mes}-${dia}`;
+
+        const relatorios = await relatorioData.getRelatoriosByData(dataFormatada);
+        return relatorios;
+    }
+
+    async excluirRelatorio(
+        id: number,
+        solicitado_por: number,
+        confirmado_por_medico: number,
+        motivo_exclusao: string
+    ): Promise<void> {
+
+        if(!solicitado_por || confirmado_por_medico) {
+            throw new Error("Campos 'solicitado_por' e 'confirmado_por_medico' são obrigatorios.")
+        }
+        if(!motivo_exclusao || motivo_exclusao.trim(). length < 30) {
+            throw new Error("O campo motivo_exclusao é obrigatorio e deve conter no minimo 30 caracteres.")
+        }
+
+        const relatorioExistente = await relatorioData.getRelatorioById(id);
+        if(!relatorioExistente) {
+            throw new Error("Relatorio não encontrado.");
+        }
+
+        await relatorioData.deleteRelatorio(id, solicitado_por, confirmado_por_medico, motivo_exclusao);
+    }
 }
