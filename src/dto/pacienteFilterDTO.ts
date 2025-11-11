b01debc2e908b245305d24c6205de0d3b446ba33
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2025-11-10 23:03:28 -0300

    Corrigindo pacienteFilterDTO.ts

--- src/dto/pacienteFilterDTO.ts
+++ src/dto/pacienteFilterDTO.ts
@@ -2,15 +2,7 @@ import { PaginationParams } from "./paginationDTO";
 
 export interface PacienteFilterDTO extends PaginationParams {
   nome?: string;
-  idade?: number;
-  peso?: number;
-  altura?: number;
-  temperatura?: number;
-  indice_glicemico?: number;
-  pressao_arterial?: string;
-  saturacao?: number;
-  pulso?: number;
-  respiracao?: number;
   sortBy?: "id" | "nome" | "idade" | "peso" | "altura" | "temperatura" | "indice_glicemico" | "pressao_arterial" | "saturacao" | "pulso" | "respiracao";
   sortOrder?: "asc" | "desc";
 }
+
