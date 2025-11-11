dcb082a463f5bc1c12c913229fec0e9e80749b80
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2025-11-11 15:17:53 -0300

    Adicionando authController.ts

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,1 +1,18 @@
+import { Request, Response } from 'express';
+import { AuthBusiness } from '../business/authBusiness';
+import { LoginInput } from '../dto/authDTO';
 
+export class AuthController {
+    authBusiness = new AuthBusiness();
+
+    public login = async (req: Request, res: Response) => {
+        try {
+            const { registro_profissional, senha } = req.body as LoginInput;
+            const result = await this.authBusiness.login({registro_profissional, senha});
+
+            res.status(200).send(result);
+        } catch (error: any) {
+            res.status(400).send({ error: error.message });
+        }
+    };
+}
