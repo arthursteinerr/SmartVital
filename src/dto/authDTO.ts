3c62c7d3bda583fc12a02a4af84942c31300092c
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2025-11-11 14:29:21 -0300

    Implementando authDTO.ts

--- src/dto/authDTO.ts
+++ src/dto/authDTO.ts
@@ -1,1 +1,15 @@
+export interface LoginInput {
+  registro_profissional: string;
+  senha: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  agente: {
+  id: number;
+  nome: string;
+  registro_profissional: string;
+  cargo: string;
+  }
+}
 
