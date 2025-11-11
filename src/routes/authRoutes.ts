347386a9d2b38136189f53ec538412cdd54d2c41
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2025-11-11 16:40:32 -0300

    Adicionando authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,1 +1,8 @@
+import express from 'express';
+import { AuthController } from '../controller/AuthController';
 
+export const authRouter = express.Router();
+
+const authController = new AuthController();
+
+authRouter.post('/login', authController.login);
