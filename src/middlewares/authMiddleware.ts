38a4f07270bf9ecd07db32bb5e8254f45022d8e5
mar 🌺 <99049408+miwagrr@users.noreply.github.com>
2025-11-11 17:18:56 -0300

    Adicionando authMiddleware.ts

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,1 +1,41 @@
+import { Request, Response, NextFunction } from 'express';
+import { authUtils } from '../utils/authUtils';
 
+declare global {
+    namespace Express {
+        interface Request {
+            agente?: {
+                id: number;
+                cargo: string;
+                registro_profissional: string;
+            };
+        }
+    }
+}
+
+export class AuthMiddleware {
+    static authenticate(req: Request, res: Response, next: NextFunction) {
+        try {
+            const authHeader = req.headers.authorization;
+
+            if (!authHeader) {
+                return res.status(401).send({ error: 'Token não fornecido' });
+            }
+
+            const token = authHeader.replace("Bearer ", "");
+
+            if (!token) {
+                return res.status(401).send({ error: 'Token mal formatado' });
+            }
+
+            const payload = authUtils.verifyToken(token);
+
+            req.agente = payload;
+
+            next();
+
+        } catch (error: any) {
+            return res.status(401).send({ error: 'Token inválido ou expirado' });
+        }
+    }
+}
